850e1a3af913d2074344630bb21922514e03d493
Ally <allyrvargas@gmail.com>
2017-12-11 19:26:45 -0800

    checkpoint 26 users associated with posts

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -13,32 +13,26 @@ class TopicsController < ApplicationController
   end
 
   def create
-    @topic = Topic.new
-    @topic.name = params[:topic][:name]
-    @topic.description = params[:topic][:description]
-    @topic.public = params[:topic][:public]
+    @topic = Topic.new(topic_params)
 
     if @topic.save
-       redirect_to @topic, notice: "Topic was saved successfully."
-     else
-       flash.now[:alert] = "Error creating topic. Please try again."
-       render :new
-     end
+      redirect_to @topic, notice: "Topic was saved successfully."
+    else
+      flash.now[:alert] = "Error creating topic. Please try again."
+      render :new
+    end
    end
 
    def edit
-     @topic = Topic.find(params[:id])
+    @topic = Topic.find(params[:id])
    end
 
    def update
     @topic = Topic.find(params[:id])
-
-    @topic.name = params[:topic][:name]
-    @topic.description = params[:topic][:description]
-    @topic.public = params[:topic][:public]
+    @topic.assign_attributes(topic_params)
 
     if @topic.save
-       flash[:notice] = "Topic was updated."
+      flash[:notice] = "Topic was updated."
       redirect_to @topic
     else
       flash.now[:alert] = "Error saving topic. Please try again."
@@ -56,6 +50,10 @@ class TopicsController < ApplicationController
       flash.now[:alert] = "There was an error deleting the topic."
       render :show
     end
-  end
-  
+  end 
+
+  private
+  def topic_params
+    params.require(:topic).permit(:name, :description, :public)
+  end 
 end
