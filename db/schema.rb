3e3bbfc8f1fe3f9f44314efa6f8e6a3d62f3d7a0
Ally <allyrvargas@gmail.com>
2017-12-15 11:09:19 -0800

    checkpoint 27 authorizations added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171212025223) do
+ActiveRecord::Schema.define(version: 20171215183622) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string "title"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 20171212025223) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -38,10 +38,19 @@ posts = Post.all
 	)
 end
 
-user = User.first
-user.update_attributes!(
-	email: 'allyrvargas@gmail.com', # replace this with your personal email
-	password: 'helloworld'
+#Create an admin user
+admin = User.create!(
+	name: 'Admin User',
+	email: 'admin@example.com',
+	password: 'helloword',
+	role: 'admin' 
+)
+
+#Create a member
+member = User.create!(
+	name: 'Member User',
+   	email: 'member@example.com',
+   	password: 'helloworld'
 )
 
 puts "Seed Finished"
